0f48d0116e01a1e26f55690999b4176e222c9a1d
Mayur Sehgal <its.mayur718@gmail.com>
2024-01-11 15:47:25 +0530

    Login Backend done upnext Sessions integration

---
+++ src/app/lib/api/login/route.ts
@@ -0,0 +1,59 @@
+import { NextResponse } from "next/server";
+import { db } from "../../utils/db";
+import { compare } from "bcrypt";
+
+async function checkUser(email: string, password: string) {
+  const user = await db.user.findUnique({
+    where: { email: email },
+  });
+  if (typeof user?.password_hash === "string") {
+    const passwordSame = await compare(password, user.password_hash);
+    return passwordSame;
+  }
+  return false;
+}
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    // LOGIN USER FROM THE USER INPUT DATA
+
+    // USER VALIDATIONS
+
+    // Check if email already exist or not
+    const isEmailExist = await db.user.findUnique({
+      where: { email: body.email },
+    });
+    if (!isEmailExist) {
+      return NextResponse.json(
+        {
+          user: null,
+          message: "EMAIL DON'T EXIST",
+        },
+        { status: 409 }
+      );
+    }
+
+    // Checking the hash password with given password
+
+    const isPasswordSame = await checkUser(body.email, body.password);
+    if (!isPasswordSame) {
+      return NextResponse.json(
+        {
+          user: null,
+          message: "AUTH FAILED : PLEASE TRY AGAIN",
+        },
+        { status: 401 }
+      );
+    }
+
+    return NextResponse.json(
+      {
+        message: "LOGGED IN SUCCESSFULLY",
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+}
